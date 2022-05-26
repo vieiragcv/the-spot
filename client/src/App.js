8d99cabe0d47e1f7862b6e36a9b635f36cace755
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-05-26 19:57:07 -0400

    file structure defined for server and client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { 
+  ApolloProvider, 
+  ApolloClient, 
+  InMemoryCache, 
+  createHttpLink, 
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
 function App() {
   return (
---
+++ client/src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Auth from '../utils/auth'
+
+
+const Login = () => {
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Login;
