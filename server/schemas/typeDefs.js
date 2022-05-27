1dcbaced356233524b027f4b8f4318f57430e2c1
Blerand Ismaili <94268979+BlerandIsm@users.noreply.github.com>
2022-05-26 22:34:48 -0400

    typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,5 +3,44 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 
-const typeDefs = gql
+const typeDefs = gql`
 
+type User {
+    _id: ID
+    username: String
+    email: String
+    category: String
+    location: String
+    downloadURL: String
+    description: String
+    preferences: String
+}
+
+type Preferences {
+    _id: ID
+    preferencesText: String
+    username: String   
+}
+
+type Auth {
+    token: ID!
+    user: User
+  }
+  
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    preferences(username: String): [User]
+    preferences(username: String): [POI]
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addPreferences(username: String! preferencesText: String!): Preferences
+    addFriend(friendId: ID!): User
+}
+`;
+
+module.exports = typeDefs
