affb13b3635c867ba8dc7b8bfd240967066d7961
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-05-27 22:13:29 -0400

    functioning front-end for sign up and login

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -11,8 +11,9 @@ const Header = () => {
   return (
     <header className="flex-row align-center container " >
       <div className="flex-row justify-space-between-sm justify-center align-center">
-        <Link to='/' className="col-sm-1"><h1>The Spot</h1></Link>
+        
         <nav className='text-center'>
+          <Link to='/'><h1>The Spot</h1></Link>
           <Link to='/login'>Login</Link>
           <Link to='/signup'>Signup</Link>
         </nav>
