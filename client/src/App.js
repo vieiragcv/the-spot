28a5c0dddf7633449da9749ed3802b4ba053a2b4
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-05-27 20:57:24 -0400

    initial css

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,8 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-import Header from './components/Header';
 
+import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -33,28 +33,29 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+
 function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div className=''>
+        <div className="flex-column justify-flex-start min-100-vh">
           <Header />
-          <div className=''>
-            <Routes>
-              <Route
-                path='/'
-                element={<Home />}
-              />
-              <Route
-                path='/login'
-                element={<Login />}
-              />
-              <Route
-                path='/signup'
-                element={<Signup />}
-              />
-            </Routes>
-          </div>
+            <div className="container">
+              <Routes>
+                    <Route
+                      path='/'
+                      element={<Home />}
+                    />
+                    <Route
+                      path='/login'
+                      element={<Login />}
+                    />
+                    <Route
+                      path='/signup'
+                      element={<Signup />}
+                    />
+              </Routes>    
+            </div>   
         </div>
       </Router>
     </ApolloProvider>
